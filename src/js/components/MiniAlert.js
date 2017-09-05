b17cdd7de3256fa9fe43b288119d96c058bb6a4a
César Guadarrama <cesarguadarrama@icloud.com>
2017-09-05 18:51:06 -0500

    💅

--- src/js/components/MiniAlert.js
+++ src/js/components/MiniAlert.js
@@ -1,16 +1,45 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function MiniAlert(props) {
+  const getClass = type => {
+    switch (type) {
+      case 0:
+        return 'energy'
+      default:
+        return 'generic'
+    }
+  }
+
+  const getText = type => {
+    switch (type) {
+      case 0:
+        return 'Energía'
+      default:
+        return 'Alerta'
+    }
+  }
+
   return (
-    <div className="mini-alert" key={alert}>
-      <div></div>
+    <div className={`mini-alert ${getClass(props.type)}`} key={alert}>
       <div>
-        <p>{props.type}</p>
-        <p>Torre {props.site}. Zona {props.zone}.</p>
+        <p className="location">Torre {props.site.name}. {props.subzone.name ? `Subzona ${props.subzone.name}. ` : ''} Zona {props.zone.name}.</p>
+        <div className="status">
+          <p className="type">{getText(props.type)}</p>
+          <p className="value">{props.report.sensor.state.value}</p>
+        </div>
       </div>
       <div></div>
     </div>
   )
+}
+
+MiniAlert.propTypes = {
+  type: PropTypes.number,
+  site: PropTypes.object.isRequired,
+  zone: PropTypes.object.isRequired,
+  subzone: PropTypes.object,
+  report: PropTypes.object.isRequired
 }
 
 export default MiniAlert
