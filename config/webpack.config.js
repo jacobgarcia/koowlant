7a4081609662d826cc14e66547edbdf16a8f9732
César Guadarrama <cesarguadarrama@icloud.com>
2017-09-05 18:45:39 -0500

    🎉 Added stage 0 for destructuring

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015', 'react', 'stage-0']
         }
       },
       {
