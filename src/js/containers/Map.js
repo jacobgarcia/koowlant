bd200492f999ef86d66a65763203b84dd33cd8ba
César Guadarrama <cesarguadarrama@icloud.com>
2017-09-07 09:56:41 -0500

    🔧 Added https to maps tiles

--- src/js/containers/Map.js
+++ src/js/containers/Map.js
@@ -380,7 +380,7 @@ class MapView extends Component {
                 )
               }
               <TileLayer
-                url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               />
             </Map>
