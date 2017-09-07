b482ff209cf2ed08f72d9a76b431819d373bbb0e
César Guadarrama <cesarguadarrama@icloud.com>
2017-09-07 03:23:13 -0500

    🎛

--- src/js/components/ZoneDetail.js
+++ src/js/components/ZoneDetail.js
@@ -20,34 +20,49 @@ class ZoneDetail extends Component {
     this.setState({ viewStyle })
   }
 
-  onHover(subZoneId) {
-    console.log(subZoneId)
+  onHover(subzoneId) {
+    console.log(subzoneId)
   }
 
   render() {
-    const { completeStatus, normalPercentage } = getStatus(this.props.zone.status)
+    console.log(this.props)
+    const { completeStatus, normalPercentage } = this.props.isZone ? getStatus(this.props.zone.status) : { completeStatus: '', normalPercentage: ''}
 
     return (
       <div className="side-content">
         <div className="top">
-          <span className="back"><Link to="/">Regresar</Link></span>
-          { this.props.isWindow === 'zones' ? null : <span className="pop-window">Hacer ventana</span> }
+          <span className="back"><Link to={`${this.props.isSubzone ? `/zones/${this.props.zone._id}` : '/'}`}>Regresar</Link></span>
+          { this.props.isWindow === 'zones' ? null : <Link to="/"><span className="pop-window">Hacer ventana</span></Link> }
         </div>
         <StatusOverall
           status={completeStatus}
+          zone={this.props.zone || this.props.subzone}
+          type={this.props.isZone ? 'zone' : 'subzone'}
         />
         <div>
           <div>
             Sensores
             Cámaras
           </div>
-          <ZonesContainer
-            zones={this.props.zone.subZones}
-            changeSitesView={this.changeSitesView}
-            viewStyle={this.state.viewStyle}
-            onHover={this.onHover}
-            subZones
-          />
+          {
+            this.props.isZone
+            ? <ZonesContainer
+                zone={this.props.zone}
+                zones={this.props.zone.subzones}
+                changeSitesView={this.changeSitesView}
+                viewStyle={this.state.viewStyle}
+                onHover={this.onHover}
+                isZone
+              />
+            : <ZonesContainer
+                subzone={this.props.subzone}
+                sites={this.props.subzone.sites}
+                changeSitesView={this.changeSitesView}
+                viewStyle={this.state.viewStyle}
+                onHover={this.onHover}
+                isSubzone
+              />
+          }
         </div>
       </div>
     )
