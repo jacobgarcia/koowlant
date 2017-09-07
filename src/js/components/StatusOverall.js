ac2e3849a7b4f794ef73d73997c83f2dc4be5ea7
César Guadarrama <cesarguadarrama@icloud.com>
2017-09-07 03:21:59 -0500

    🎛 To be used in multiple detail views

--- src/js/components/StatusOverall.js
+++ src/js/components/StatusOverall.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import {StatusBar} from './'
 
 function StatusOverall(props) {
   const normal = props.status.length > 0 ? props.status.filter(({name}) => name === 'normal').pop() : { value: 0}
-  console.log(props)
+
+  const getTitle = (type, name) => {
+    switch (type) {
+      case 'zone': return 'Zona ' + name
+      case 'subzone': return 'Subzona ' + name
+      default: return 'Estatus General'
+    }
+  }
 
   return (
     <div className="overall">
-      <h3>Estatus general</h3>
+      <h3>{getTitle(props.type, props.zone ? props.zone.name : null)}</h3>
       <div className="sites-status">
         <StatusBar status={props.status}/>
         <p><span>{normal.value * 100}%</span> de funcionalidad, <span className="alert"></span> 18 Alertas totales</p>
@@ -16,6 +24,12 @@ function StatusOverall(props) {
       </div>
     </div>
   )
+}
+
+StatusOverall.propTypes = {
+  type: PropTypes.string.isRequired,
+  zone: PropTypes.object,
+  // status: PropTypes.object
 }
 
 export default StatusOverall
