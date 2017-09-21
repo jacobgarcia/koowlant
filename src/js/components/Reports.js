841a61cb34a7d1c6ab6e356d5312ed08db515ffd
César Guadarrama <cesarguadarrama@icloud.com>
2017-09-21 12:23:09 -0500

    🎉 Added new placeholder reports

--- src/js/components/Reports.js
+++ src/js/components/Reports.js
@@ -14,34 +14,42 @@ function Reports(props) {
       <div className="side-content">
         { props.isWindow === 'zones' ? null : <span className="pop-window">Hacer ventana</span> }
         <div className="general-alerts">
-          <h3>Alertas totales</h3>
-          <p>18</p>
-        </div>
-        <div>
-          <p>Mostrando <span>Zona A</span> <span>Eliminar selección</span></p>
-          <div>
-            <p>Alertas</p>
-            <input type="button"/>
-            <input type="button"/>
-          </div>
-        </div>
-        <div className="zone-section">
-          <h2>Zona A</h2><span>11 Alertas</span>
+          <h2>Alertas</h2>
+          <span>4 sin revisar</span>
         </div>
         <div className="mini-alerts-container">
-          {
-            props.reports &&
-            props.reports.map(report =>
-              <MiniAlert
-                key={report._id}
-                type={report.type}
-                report={report}
-                site={report.site}
-                zone={report.zone}
-                subzone={report.subZone}
-              />
-            )
-          }
+
+            <div className="mini-alert battery">
+              <div className="details">
+                <div><span>Zona A Torre 5</span></div>
+                <p>Batería baja 18%</p>
+              </div>
+              <div className="time">
+                <span>23/05/2017</span>
+                <span>20:24</span>
+              </div>
+            </div>
+            <div className="mini-alert temperature">
+              <div className="details">
+                <div><span>Zona A Torre 5</span></div>
+                <p>Temperatura alta 70°</p>
+              </div>
+              <div className="time">
+                <span>23/05/2017</span>
+                <span>20:24</span>
+              </div>
+            </div>
+            <div className="mini-alert fuel">
+              <div className="details">
+                <div><span>Zona A Torre 5</span></div>
+                <p>Combustible bajo 15%</p>
+              </div>
+              <div className="time">
+                <span>23/05/2017</span>
+                <span>20:24</span>
+              </div>
+            </div>
+
         </div>
       </div>
     </div>
