a0122282fe1b9799ada82f8827fdfa7db3af9670
Jacob Garcia <rock2494@hotmail.com>
2017-11-07 16:29:56 -0600

    Endpoint for state retrieval

--- router/v1/polygons.js
+++ router/v1/polygons.js
@@ -22,3 +22,20 @@ router.route('/polygons/:country/:state/coordinates')
     return res.status(200).json({ state.positions })
   })
 })
+
+router.route('/polygons/:country/states')
+.get((req, res) => {
+  const { country } = req.params
+  //TODO: Country
+  State.find({ })
+  .exec((error, states) => {
+    if (error) {
+      winston.error({error})
+      return res.status(500).json({ error })
+    }
+
+    if (!states) return res.status(404).json({ message: 'No state found'})
+
+    return res.status(200).json({ states })
+  })
+})
