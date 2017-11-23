23c095da8bf5e046d0e77e53d7fef96f3596bfb4
César Guadarrama Cantú <cesarguadarrama@icloud.com>
2017-11-22 20:00:13 -0600

    Changed routes and stuff

--- src/js/router.js
+++ src/js/router.js
@@ -8,8 +8,8 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
 import Login from './containers/Login'
-import Signup from './containers/Signup'
-import Streaming from './components/Stream'
+// import Signup from './containers/Signup'
+// import Streaming from './components/Stream'
 
 import App from './containers/App'
 
@@ -17,23 +17,31 @@ import appReducer from './reducers'
 
 const store = createStore(appReducer)
 
-const videoJsOptions = {
-  autoplay: true,
-  controls: false,
-  sources: [{
-    src: 'rtmp://demo.kawlantid.com/live&idiots',
-    type: 'rtmp/mp4'
-  }]
+// Hot reload fix
+if (module.hot) {
+  // Enable Webpack hot module replacement for reducers
+  module.hot.accept('./reducers', () => {
+    const nextRootReducer = require('./reducers')
+    store.replaceReducer(nextRootReducer)
+  })
 }
+
+// const videoJsOptions = {
+//   autoplay: true,
+//   controls: false,
+//   sources: [{
+//     src: 'rtmp://demo.kawlantid.com/live&idiots',
+//     type: 'rtmp/mp4'
+//   }]
+// }
 
 function Routes() {
   return (
     <Provider store={store}>
       <Router>
         <Switch>
-          <Route path="/streaming" render={() => <Streaming { ...videoJsOptions } />} />
           <Route path="/login" component={Login}/>
-          <Route path="/signup/:invitation_token" component={Signup}/>
+          {/* <Route path="/signup/:invitation_token" component={Signup}/> */}
           <Route path="/" component={App} />
         </Switch>
       </Router>
