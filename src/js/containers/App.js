3a27c4ab5f036efc763522f60211d435d9fed595
César Guadarrama Cantú <cesarguadarrama@icloud.com>
2017-11-22 20:02:54 -0600

    Added login

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -3,23 +3,90 @@ import { Switch, Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-// import { Nav } from '../components'
-// import { NetworkOperation } from '../lib/NetworkOperation'
+import { setCredentials, setComplete, setLoading, setExhaustive } from '../actions'
+import { NetworkOperation } from '../lib'
+import { Map } from './'
+import { Nav } from '../components'
+import io from 'socket.io-client'
 
 class App extends Component {
   componentWillMount() {
-    // TODO Verify auth
-    // TODO Get user
+    const token = localStorage.getItem('token')
+    const path = `${this.props.location.pathname}${this.props.location.search}`
+
+    if (!token) {
+      localStorage.removeItem('token')
+      this.props.history.replace(`/login?return=${path}`)
+      return
+    }
+
+    this.props.setLoading()
+
+    NetworkOperation.getSelf()
+    .then(({data}) => {
+      this.props.setCredentials({...data.user, token})
+
+      // Start socket connection
+      this.initSocket(this.props, token)
+
+      return NetworkOperation.getExhaustive()
+    })
+    .then(({data}) => {
+      // Set all zones
+      this.props.setExhaustive(data.zones)
+
+      this.props.setComplete()
+    })
+    .catch(({response}) => {
+      this.props.setComplete()
+
+      switch (response.status) {
+        case 401:
+        case 400:
+          this.props.history.replace(`/login?return=${path}`)
+          break
+        default:
+          // TODO Display error
+          break
+      }
+    })
+  }
+
+  componentDidCatch(error, info) {
+    // TODO Display fallback UI
+    console.log('APP ERROR', error)
+    // TODO You can also log the error to an error reporting service
+    // logErrorToMyService(error, info)
+  }
+
+  initSocket(props, token) {
+    this.socket = io(`https://demo.kawlantid.com`)
+    this.socket.connect()
+
+    this.socket.emit('join', token)
+
+    this.socket.on('connect', () => {
+      this.socket.emit('join', token)
+    })
+
+    this.socket.on('report', report => {
+      console.log('GOT REPORT')
+      props.setReport(report)
+    })
   }
 
   render() {
+    const { props } = this
     return (
       <div id="app">
-        {/* <Nav /> */}
-        <h1>React Boilerplate App</h1>
-        <p>React, Redux, HotReload, Axios, React Router, Express and more</p>
+        <Nav
+          pathname={props.location.pathname}
+          loading={props.loading}
+          credentials={props.credentials}
+        />
         <Switch>
-          {/* TODO Add routes */}
+          <Route exact path="/" component={Map} />
+          <Route path="/zones/:zoneId?/:subzoneId?/:siteId?" component={Map} />
         </Switch>
       </div>
     )
@@ -27,14 +94,35 @@ class App extends Component {
 }
 
 App.propTypes = {
+  setCredentials: PropTypes.func,
+  setComplete: PropTypes.func,
+  setLoading: PropTypes.func,
+  history: PropTypes.object,
+  setExhaustive: PropTypes.func
 }
 
 function mapDispatchToProps(dispatch) {
-  return {}
+  return {
+    setCredentials: user => {
+      dispatch(setCredentials(user))
+    },
+    setLoading: () => {
+      dispatch(setLoading())
+    },
+    setComplete: () => {
+      dispatch(setComplete())
+    },
+    setExhaustive: zones => {
+      dispatch(setExhaustive(zones))
+    }
+  }
 }
 
-function mapStateToProps() {
-  return {}
+function mapStateToProps({loading, credentials}) {
+  return {
+    loading,
+    credentials
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
